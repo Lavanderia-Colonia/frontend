a6e73a1735b37b6b4998c851206140260d783536
DiogoSAbr <diogosantanadealmeida@gmail.com>
2025-11-27 07:28:15 -0300

    fix: adjusting RegisterOrder page

--- app/pedidos/cadastrar/page.tsx
+++ app/pedidos/cadastrar/page.tsx
@@ -1,5 +1,14 @@
+"use client";
+
 import RegisterOrder from "@/components/RegisterOrder";
+import { IPedidos } from "@/models/pedidos";
+import { useState } from "react"; 
 
 export default function Page() {
-    return <RegisterOrder />;
+    const [pedido, setPedido] = useState<IPedidos>({tipoFinalizacao: "",
+        prazo: "",
+        cliente: null,
+        products: []});
+
+    return <RegisterOrder pedido={pedido} setPedido={setPedido} />;
 }
